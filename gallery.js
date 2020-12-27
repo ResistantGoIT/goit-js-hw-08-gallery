86bd930db4439badc1906a96865c721000f53c94
Vitaliy Protasevych <vitaliy.protasevych@gmail.com>
2020-12-27 22:35:54 +0200

    additional

--- gallery.js
+++ gallery.js
@@ -4,6 +4,8 @@ const galleryListItem = document.querySelector(".js-gallery");
 const modal = document.querySelector(".js-lightbox");
 const modalPhoto = document.querySelector(".lightbox__image");
 const modalClose = document.querySelectorAll(".js-close-modal");
+const btnLeft = document.querySelector(".lightbox__button-left");
+const btnRight = document.querySelector(".lightbox__button-right");
 
 const toHTML = (photo) => `
 <li class="gallery__item">
@@ -20,21 +22,26 @@ const toHTML = (photo) => `
   </a>
 </li>
 `;
+
+//Добавление каждого объекта массива в галерею
 function render() {
   const html = galleryItems.map(toHTML).join("");
   galleryListItem.innerHTML = html;
 }
 render();
 
+//Открытие модального окна нажатием на картинку
+let imgSource;
 galleryListItem.addEventListener("click", (event) => {
   event.preventDefault();
-  const imgSource = event.target.dataset.source;
+  let imgSource = event.target.dataset.source;
   if (imgSource) {
     modalPhoto.src = imgSource;
     modal.classList.add("is-open");
   }
 });
 
+//Закрытие модального окна кликом на оверлэй или крестик
 modalClose.forEach((el) => {
   el.addEventListener("click", (event) => {
     event.preventDefault();
@@ -42,3 +49,24 @@ modalClose.forEach((el) => {
     modalPhoto.src = " ";
   });
 });
+
+//Закрытие модального окна кнопкой 'Escape'
+window.addEventListener("keydown", onKeydown);
+function onKeydown(event) {
+  if (event.code === "Escape") {
+    modal.classList.remove("is-open");
+  }
+}
+
+for (let photo = 0; photo < galleryItems.length; photo += 1) {
+  btnLeft.addEventListener("click", (event) => {
+    if (photo > 1 && photo < 10) {
+      modalPhoto.src = galleryItems[(photo -= 1)].original;
+    }
+  });
+  btnRight.addEventListener("click", (event) => {
+    if (photo > 1 && photo < 8) {
+      modalPhoto.src = galleryItems[(photo += 1)].original;
+    }
+  });
+}
